f252bb6efde13110d8d3eafb18e534a53d6c0892
hack0001 <TynyCoed@Thomas-Patels-iMac.local>
2019-05-08 16:49:04 +0100

    new aws zone check

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 export default () => (
   <div style={{ color: `purple` }}>
     <Link to="about">ABOUT</Link>
-    <h1>Gatsby Automated Deployment!</h1>
+    <h1>Final GAtsby Check</h1>
     <p>What a world.</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>
