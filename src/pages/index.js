e5c9bcd90e1c4cab23be29835034938d569b2bf9
hack0001 <TynyCoed@Thomas-Patels-iMac.local>
2019-05-08 15:46:39 +0100

    testing automated deployment

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 export default () => (
   <div style={{ color: `purple` }}>
     <Link to="about">ABOUT</Link>
-    <h1>Hello Gatsby!</h1>
+    <h1>Gatsby Automated Deployment!</h1>
     <p>What a world.</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>
